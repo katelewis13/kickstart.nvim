37b58b6ea5f43848186eefa4b9dd2bc06cdacbed
Kate <kate@scai.group>
2024-12-06 15:54:57 +1100

    indent line, harpoon, legendary etc

--- init.lua
+++ init.lua
@@ -287,29 +287,6 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
-    end,
-  },
-
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
@@ -602,7 +579,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -693,6 +670,7 @@ require('lazy').setup({
         typescriptreact = { { 'prettierd', 'prettier' } },
         -- html = { { 'prettier' } },
         css = { { 'prettier' } },
+        go = { { 'gofmt' } },
       },
     },
   },
@@ -813,15 +791,46 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
 
-      -- You can configure highlights by doing something like:
+    'folke/tokyonight.nvim',
+    priority = 1000,
+    config = function()
+      local util = require 'tokyonight.util'
+      require('tokyonight').setup {
+        style = 'night',
+        styles = {
+          functions = {},
+        },
+        plugins = {
+          all = true,
+          auto = false,
+          ['render-markdown.nvim'] = true,
+        },
+        on_colors = function(colors)
+          -- These color modifications might not directly affect Markdown headers
+        end,
+
+        on_highlights = function(hl, c)
+          -- Base highlights from the plugin
+          hl.RenderMarkdownBullet = { fg = c.orange }
+          hl.RenderMarkdownCode = { bg = c.bg_dark }
+          hl.RenderMarkdownDash = { fg = c.orange }
+          hl.RenderMarkdownTableHead = { fg = c.red }
+          hl.RenderMarkdownTableRow = { fg = c.orange }
+          hl.RenderMarkdownCodeInline = '@markup.raw.markdown_inline'
+
+          -- Heading highlights using rainbow colors
+          -- c.rainbow is an array of colors that tokyonight uses for rainbow highlighting
+          for i, color in ipairs(c.rainbow) do
+            -- Use util.blend_bg to create a semi-transparent background color
+            hl['RenderMarkdownH' .. i .. 'Bg'] = { bg = util.blend_bg(color, 0.1) }
+            -- Set the foreground color for the heading
+            hl['RenderMarkdownH' .. i] = { fg = color, bold = true }
+          end
+        end,
+      }
+      -- Move the colorscheme setting here, after the plugin is loaded and configured
+      vim.cmd.colorscheme 'tokyonight-night'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
@@ -847,20 +856,23 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      local indentscope = require 'mini.indentscope'
+      indentscope.setup { draw = { delay = 0, animation = indentscope.gen_animation.none() } }
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
+      -- local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- statusline.setup { use_icons = vim.g.have_nerd_font }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      -- statusline.section_location = function()
+      -- return '%2l:%-2v'
+      --end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
@@ -878,6 +890,7 @@ require('lazy').setup({
         'lua',
         'luadoc',
         'markdown',
+        'markdown_inline',
         'vim',
         'vimdoc',
         'c_sharp',
@@ -888,6 +901,7 @@ require('lazy').setup({
         'tsx',
         'typescript',
         'javascript',
+        'go',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
@@ -940,6 +954,7 @@ require('lazy').setup({
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
 
+  { import = 'plugins' },
   --  {
   --    'nvim-tree/nvim-tree.lua',
   --    version = '*',
--- lua/custom/plugins/legendary.lua
+++ lua/custom/plugins/legendary.lua
@@ -4,6 +4,9 @@ return {
   -- its recommended to load legendary.nvim before other plugins
   priority = 10000,
   lazy = false,
+  config = function()
+    require('legendary').setup { extensions = { lazy_nvim = true } }
+  end,
   -- sqlite is only needed if you want to use frecency sorting
   -- dependencies = { 'kkharji/sqlite.lua' }
 }
